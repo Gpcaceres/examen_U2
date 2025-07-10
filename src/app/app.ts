22e8a5d34e013a7489ecebf63f2091d52a32f842
Germán Cáceres <gpcaceres@espe.edu.ec>
2025-07-10 15:13:26 -0500

    Fix .gitignore and untrack node_modules

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected title = 'Caceres_German_Ex2_C';
+  speed = 0;
+
+  acelerar(): void {
+    this.speed += 5;
+  }
+
+  frenar(): void {
+    this.speed -= 3;
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+  }
+
+  reiniciar(): void {
+    this.speed = 0;
+  }
+
+  getVelocidad(): string {
+    if (this.speed >= 0 && this.speed <= 30) {
+      return 'low-speed';
+    } else if (this.speed > 30 && this.speed <= 70) {
+      return 'medium-speed';
+    } else if (this.speed > 70 && this.speed <= 120) {
+      return 'high-speed';
+    } else {
+      return 'danger-speed';
+    }
+  }
 }
